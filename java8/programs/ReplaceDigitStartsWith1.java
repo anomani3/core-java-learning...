fe5a942cdefa4da4b62d041132bb674d9d405644
Ashraf Nomani <anomanijd@gmail.com>
2026-07-16 11:15:10 +0530

    Added starts with 1 end with 3 and count specific filtered value also used logic for string output and integer output

--- java8/programs/ReplaceDigitStartsWith1.java
+++ java8/programs/ReplaceDigitStartsWith1.java
@@ -2,13 +2,53 @@ package ashraf.java8.programs;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReplaceDigitStartsWith1 {
     public static void main(String[] args) {
-        List<Integer> lst= Arrays.asList(12,11,34,56,77,112);
+        List<Integer> lst= Arrays.asList(12,11,34,56,77,112,74);
         lst.stream()
                 .map(String::valueOf)
                 .filter(n->n.startsWith("1"))
                 .forEach(System.out::println);
+
+//        Numbere ends with 4
+
+        System.out.println("emds with logic using collecto to list store in list of String");
+
+    List<String>res=    lst.stream()
+                .map(String::valueOf)
+                .filter(s->s.endsWith("4"))
+                .collect(Collectors.toList());
+
+        System.out.println(res);
+
+
+        System.out.println("using for each");
+
+        lst.stream()
+                .map(String::valueOf)
+                .filter(n -> n.endsWith("6"))
+                .forEach(System.out::println);
+
+
+        System.out.println("WAnt output in Integer");
+
+       List<Integer>res1= lst.stream()
+                .filter(n->String.valueOf(n).startsWith("1"))
+                .collect(Collectors.toList());
+
+        System.out.println(res1);
+
+
+        System.out.println("Count the specific filtered value");
+
+     Long res2=   lst.stream()
+                .map(String::valueOf)
+                .filter(n->n.startsWith("7"))
+                .count();
+
+        System.out.println(res2);
+
     }
 }
