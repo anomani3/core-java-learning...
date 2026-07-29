d419f312ed123685993e9e583fce0f1a54e4413c
Ashraf Nomani <anomani786@hotmail.com>
2026-07-29 06:53:32 +0530

    added employee and student for learning or revising filter

---
+++ ashraf/ashraf/ashraf/java8/streammethods_terminal_intermediate/reallifestudent_examples/Student.java
@@ -0,0 +1,4 @@
+package ashraf.ashraf.ashraf.ashraf.java8.streammethods_terminal_intermediate.reallifestudent_examples;
+
+public class Student {
+}
---
+++ ashraf/ashraf/ashraf/java8/streammethods_terminal_intermediate/reallifestudent_examples/StudentDemoQuestions.java
@@ -0,0 +1,7 @@
+package ashraf.ashraf.ashraf.ashraf.java8.streammethods_terminal_intermediate.reallifestudent_examples;
+
+public class StudentDemoQuestions {
+    public static void main(String[] args) {
+
+    }
+}
