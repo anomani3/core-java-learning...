b8d961e934d22ef7765f26e0a255b058fad31799
Ashraf Nomani <anomani786@hotmail.com>
2026-07-31 21:41:27 +0530

    some coding added for checkong

--- ashraf/ashraf/ashraf/java8/streammethods_terminal_intermediate/map/EmployeeExample.java
+++ ashraf/ashraf/ashraf/java8/streammethods_terminal_intermediate/map/EmployeeExample.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public class EmployeeExample {
     public static void main(String[] args) {
-        List<Employee> employeeList= Arrays.asList(
-                new Employee(1,"Ashraf","IT","M",12000),
-                new Employee(2,"Manju","HR","F",7000),
-                new Employee(3,"Arish","Pharmacy","M",6000),
-                new Employee(4,"Kaju","Mechanical","M",10000),
-                new Employee(5,"Raju","IT","F",3000),
+        List<Employee> employeeList = Arrays.asList(
+                new Employee(1, "Ashraf", "IT", "M", 12000),
+                new Employee(2, "Manju", "HR", "F", 7000),
+                new Employee(3, "Arish", "Pharmacy", "M", 6000),
+                new Employee(4, "Kaju", "Mechanical", "M", 10000),
+                new Employee(5, "Raju", "IT", "F", 3000),
                 new Employee(6, "Priya", "Finance", "F", 15000),
                 new Employee(7, "Rahul", "IT", "M", 18000)
         );
@@ -20,7 +20,7 @@ public class EmployeeExample {
         System.out.println("Get Employee Names");
 
         employeeList.stream()
-                .map(e->e.getName())
+                .map(e -> e.getName())
                 .forEach(System.out::println);
 
 
@@ -35,8 +35,14 @@ public class EmployeeExample {
         System.out.println("Increase salary by 10%");
 
         employeeList.stream()
-                .map(e->e.getSalary()*1.10)
+                .map(e -> e.getSalary() * 1.10)
                 .forEach(System.out::println);
 
+
+        System.out.println("Get employee department");
+
+        employeeList.stream()
+                .map(Employee::getDepartment)
+                .forEach(System.out::println);
     }
 }
