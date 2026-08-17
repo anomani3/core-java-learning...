19fb20c1734dd078fce85ce5b09ac603d3d2fb5f
Ashraf Nomani <anomani786@hotmail.com>
2026-08-17 14:28:45 +0530

    Ashraf pushed checking conflict

--- CoreJavaPractice/ashraf/java8/conflictchcking/ConflictChecking.java
+++ CoreJavaPractice/ashraf/java8/conflictchcking/ConflictChecking.java
@@ -1,3 +1,4 @@
+package CoreJavaPractice.ashraf.java8.conflictchcking;
 
 class ConflictChecking {
     public static void main(String[] args) {
@@ -5,6 +6,7 @@ class ConflictChecking {
 System.out.println("Hello Kamrudding how are you?");
 
 System.out.println("java ");
+        System.out.println("Ashraf added pushed !!");
 
     }
 }
---
+++ out/production/projects/ShellScripting/Shellscripting_project/s3.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+#how to declare a variable in bash scripting
+name="ashraf"
+age=30
+city="Bangalore"
+pin=20
+
+# for pring any value we are using echo command
+echo "My name is $name"
+echo "My age is $age city $city and pin $pin"
+
+#iam adding pin and age
+sum=$((age+pin))
+echo "The sum is $sum"
+
